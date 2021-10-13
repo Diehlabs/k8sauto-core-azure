644fa7bca6a9290fafd1fd88aea213a214a548f1
Chris Diehl <cultclassik@gmail.com>
2021-10-13 11:48:27 -0500

    update output

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,11 @@ output "ssh_key" {
   sensitive = true
 }
 
+output "ssh_key_b64" {
+  value     = base64encode(tls_private_key.ssh_key.private_key_pem)
+  sensitive = true
+}
+
 output "subnets" {
   value = {
     control = azurerm_subnet.akscontrolsub
